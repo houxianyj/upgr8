f2ad30c28702e1cc0a252c833ee39a770ffc9d66
yaojun <yaojun@madhouse-inc.com>
2020-12-23 17:55:14 +0800

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yao Jun
+ * Date: 2020/12/21
+ * Time: 14:18
+ * File name: index.php
+ */
+//xdebug_info();
+$a=123;
+$b=456;
+$c=22333;
+
+echo $a,$b;
